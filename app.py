0e6832f9bb31ac9e64ed66325f879d67ca2eee7a
Somnath kshirsagar <112411951+somanathkshirsagar@users.noreply.github.com>
2023-07-08 23:52:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ async def index():
 async def training():
     try:
         os.system("python main.py")
-        return Response("Training successful !!")
+        return Response("Training successful !!") 
 
     except Exception as e:
         return Response(f"Error Occurred! {e}")
