7678b3af9d342cd4292644f70611999a2898c522
Somnath kshirsagar <112411951+somanathkshirsagar@users.noreply.github.com>
2023-07-08 23:51:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 import sys
-import os
+import os 
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from fastapi.responses import Response
